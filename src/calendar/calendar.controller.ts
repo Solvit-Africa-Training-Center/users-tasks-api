71f716aca620561cde82f94e41fbcdc1810bb435
Jean Baptiste Niyikiza <jbniyikiza20@gmail.com>
2025-08-25 22:51:23 +0200

    swagger controller api

--- src/calendar/calendar.controller.ts
+++ src/calendar/calendar.controller.ts
@@ -10,11 +10,12 @@ import {
 } from '@nestjs/common';
 import { CalendarService } from './calendar.service';
 import { CreateCalendarDto, UpdateCalendarDto } from './calender.dto';
-import { ApiTags, ApiOkResponse, ApiCreatedResponse } from '@nestjs/swagger';
+import { ApiTags, ApiOkResponse, ApiCreatedResponse, ApiBearerAuth,ApiUnauthorizedResponse,ApiNotFoundResponse, } from '@nestjs/swagger';
 import { UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @ApiTags('Calendar')
+@ApiBearerAuth()
 @Controller('calendar')
 @UseGuards(AuthGuard('jwt'))
 export class CalendarController {
@@ -22,24 +23,30 @@ export class CalendarController {
 
   @Post()
   @ApiCreatedResponse({ description: 'Event successfully created' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
   create(@Body() dto: CreateCalendarDto, @Req() req: any) {
     return this.calendarService.create(dto, req.user.userId);
   }
 
   @Get()
   @ApiOkResponse({ description: 'List of calendar events' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
   findAll(@Req() req: any) {
     return this.calendarService.findAll(req.user.userId, req.user.role);
   }
 
   @Get(':id')
   @ApiOkResponse({ description: 'Single event details' })
+  @ApiNotFoundResponse({ description: 'Event not found' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
   findOne(@Param('id') id: string, @Req() req: any) {
     return this.calendarService.findOne(id, req.user.userId, req.user.role);
   }
 
   @Patch(':id')
   @ApiOkResponse({ description: 'Event successfully updated' })
+  @ApiNotFoundResponse({ description: 'Event not found' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
   update(
     @Param('id') id: string,
     @Body() dto: UpdateCalendarDto,
@@ -50,6 +57,8 @@ export class CalendarController {
 
   @Delete(':id')
   @ApiOkResponse({ description: 'Event successfully deleted' })
+  @ApiNotFoundResponse({ description: 'Event not found' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
   remove(@Param('id') id: string, @Req() req: any) {
     return this.calendarService.remove(id, req.user.userId, req.user.role);
   }
